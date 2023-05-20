00d2078f3df4e4209c8be3b1af6634ad41e96663
robertwby87 <robertwby@gmail,com>
2023-05-20 22:44:15 +0800

    Use better API

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const authorText = document.getElementById('author');
 
 // Get Quotes from API
 const getRandomAPIQuote = async () => {
-    const apiUrl = "https://zenquotes.io/api/random";
+    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
     const response = await fetch(apiUrl);
     const apiQuotes = await response.json()
 
-    quoteText.textContent = apiQuotes[0]['q'];
-    authorText.textContent = apiQuotes[0]['a'];
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+    quoteText.textContent = quote['text'];
+    authorText.textContent = quote['author'];
 }
 
 // Tweet Quote
+
+
 
 nextButton.addEventListener('click', getRandomAPIQuote);
 
