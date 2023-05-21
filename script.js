6a8f07dbff8e5afc28e35a414392805b9504dd27
robertwby87 <robertwby@gmail,com>
2023-05-21 22:40:22 +0800

    Finalize the app

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const quoteContainer = document.getElementById('quote');
 const nextButton = document.getElementById('next-button');
+const twitterButton = document.getElementById('tweet-button');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
+const loader = document.getElementById('loader');
+
+
+let apiQuotes = [];
+
+// show Loader
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//  Hide Loader
+function removeLoadingSpinner() {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
 
 // Get Quotes from API
 const getRandomAPIQuote = async () => {
-    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-    const response = await fetch(apiUrl);
-    const apiQuotes = await response.json()
+    showLoadingSpinner();
+    try {
+        const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+    } catch (error) {
+        alert(error);
+    }
+    removeLoadingSpinner();
+}
 
+// Show Quote onto the card
+const showQuote = () => {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
     quoteText.textContent = quote['text'];
     authorText.textContent = quote['author'];
 }
 
-// Tweet Quote
+//  Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
 
 
+nextButton.addEventListener('click', showQuote);
+twitterButton.addEventListener('click',tweetQuote);
 
-nextButton.addEventListener('click', getRandomAPIQuote);
-
+removeLoadingSpinner();
+getRandomAPIQuote();
 
